6fd8470f10a671433bd5773c6c6768fc744fd225
PckitDragonlord <jonklement@yahoo.com>
2025-07-22 21:29:32 -0500

    new fresh three

--- firebase.js
+++ firebase.js
@@ -1,9 +1,7 @@
-// firebase.js
-import firebase from "https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js";
-import "https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js";
-import "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js";
+// firebase.js — Firebase v8.10.1 configuration and initialization
 
-const firebaseConfig = {
+// Your web app's Firebase configuration
+var firebaseConfig = {
   apiKey: "AIzaSyBmthZz_uTdO1y-dAey42v9gznMqLCDQ_A",
   authDomain: "pckit-dragons-dev.firebaseapp.com",
   projectId: "pckit-dragons-dev",
@@ -11,11 +9,7 @@ const firebaseConfig = {
   messagingSenderId: "413167849496",
   appId: "1:413167849496:web:4feb00d1bf28916ac7b36d",
   measurementId: "G-YQ9C6ZB2ZP"
-
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-export const auth = firebase.auth();
-export const db = firebase.firestore();
-export { firebase };
