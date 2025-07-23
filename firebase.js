2ec97368c00f2736e16c3d92b3f56d3b99262b6a
PckitDragonlord <jonklement@yahoo.com>
2025-07-22 21:14:37 -0500

    button

--- firebase.js
+++ firebase.js
@@ -1,6 +1,8 @@
-// firebase.js - Firebase Initialization for v8.10.1
+// firebase.js
+import firebase from "https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js";
+import "https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js";
+import "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js";
 
-// Your Firebase config (same as before)
 const firebaseConfig = {
   apiKey: "AIzaSyBmthZz_uTdO1y-dAey42v9gznMqLCDQ_A",
   authDomain: "pckit-dragons-dev.firebaseapp.com",
@@ -9,9 +11,11 @@ const firebaseConfig = {
   messagingSenderId: "413167849496",
   appId: "1:413167849496:web:4feb00d1bf28916ac7b36d",
   measurementId: "G-YQ9C6ZB2ZP"
+
 };
 
-// Initialize Firebase app if not already initialized
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const db = firebase.firestore();
+export { firebase };
